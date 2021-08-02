ecbbf27685a6b54cfdb1328a122f0dd1b2c2a351
sksatu1 <satueee18@gmail.com>
2021-08-02 12:19:30 +0600

    first

---
+++ array-loop.js
@@ -0,0 +1,5 @@
+var names = ['satu', 'sajib', 'sohel', 'robin'];
+for (var i = 0; i < names.length; i++) {
+    var position = names[i];
+    console.log(position);
+}
---
+++ while-loop.js
@@ -0,0 +1,13 @@
+// var roastGiven = 0;
+// while (roastGiven < 7) {
+//     console.log(roastGiven);
+//     console.log('roast den doya kore');
+//     roastGiven++;
+// }
+
+
+var n = 2;
+while (n <= 10) {
+    console.log(n);
+    n = n + 2;
+}
